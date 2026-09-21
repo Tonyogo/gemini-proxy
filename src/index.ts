734bc085b2bc17aeb30c344e071d2428aacaaa2d
liyatao <liyatao@yogorobot.com>
2026-09-21 19:19:35 +0800

    chore: initialize accountUsageService on server startup

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ import app from './app';
 import config from '../config/default';
 import logger from './utils/logger';
 import metricsService from './admin/services/metricsService';
+import accountUsageService from './admin/services/accountUsageService';
 import { setupTerminalWebSocket } from './terminal/routes/terminalWs';
 
 const server = http.createServer(app);
 setupTerminalWebSocket(server);
 
-metricsService.init().then(() => {
+Promise.all([
+  metricsService.init(),
+  accountUsageService.init()
+]).then(() => {
   server.listen(config.port, () => {
     logger.info(`Server is running on port ${config.port}`);
     logger.info(`Proxying upstream requests to Gemini: ${config.geminiBaseUrl}`);
